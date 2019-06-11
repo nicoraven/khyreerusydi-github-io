8b6e5b90fd4bfefb19da62423c4a4788b02dcfaf
khyreerusydi <nicoraventbh@gmail.com>
2019-06-11 10:38:35 +0800

    added header

--- script.js
+++ script.js
@@ -1,25 +1,41 @@
-// Preloader
-
-// to fade out
-fade = (el) => {
-    console.log("starting", el.style.opacity);
+// to fade out an element
+fadeOut = (el) => {
+    // console.log("starting", el.style.opacity);
     if (el.style.opacity == 0) {
-        console.log("everything has loaded");
+        // console.log("everything has loaded");
     } else {
-        if ((el.style.opacity -= .1) < 0) {
+        if ((el.style.opacity -= .015) < 0) {
             el.style.display = "none";
         } else {
-            console.log(el.style.opacity);
-            requestAnimationFrame(() => { this.fade(el); });
+            // console.log(el.style.opacity);
+            requestAnimationFrame(() => { this.fadeOut(el); });
         }
     }
 };
 
+let toggleMenu = (event) => {
+    console.log("click")
+    let content = document.querySelector(".menu-content");
+    if (content.style.display === "none") {
+        console.log("opening");
+        content.style.display = "block";
+    } else {
+        console.log("closing");
+        content.style.display = "none";
+    }
+}
+
 window.onload = () => {
     // fade out preloader when everything has loaded
     let preloader = document.getElementById("preloader");
-
     preloader.style.opacity = 1;
+    fadeOut(preloader);
 
-    fade(preloader);
+    // add click listener to toggle menu
+    let menu = document.getElementById("menu");
+    menu.addEventListener("click", toggleMenu);
+    console.log("listening");
+
+    let content = document.querySelector(".menu-content");
+    // console.log(content.style.display);
 };
