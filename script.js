cc9bb12d045576810371eee4dc44774ebc4199d1
khyreerusydi <nicoraventbh@gmail.com>
2019-06-12 16:04:13 +0800

    added projects & footer

--- script.js
+++ script.js
@@ -14,28 +14,32 @@ fadeOut = (el) => {
 };
 
 let toggleMenu = (event) => {
+    console.log(event.target.tagName);
     let content = document.querySelector(".menu-content");
     if (content.style.display === "none") {
         console.log("opening");
         content.style.display = "block";
     }
-    else if (content.style.display === "block" && event.target.className === "menu-content") {
+    else if (content.style.display === "block" && event.target.className === "menu-content" || event.target.tagName === 'A' || event.target.tagName === 'ION-ICON') {
         console.log("closing", event.target.className);
         content.style.display = "none";
     }
 }
 
 window.onload = () => {
-    // fade out preloader when everything has loaded
+    // check if page has preloader
     let preloader = document.getElementById("preloader");
-    preloader.style.opacity = 1;
-    fadeOut(preloader);
+    if (preloader !== null) {
+        // fade out preloader when everything has loaded
+        preloader.style.opacity = 1;
+        fadeOut(preloader);
+    };
 
     // add click listener to toggle menu
     let menu = document.getElementById("menu");
     menu.addEventListener("click", toggleMenu);
-    console.log("listening");
+    // console.log("listening");
 
     let content = document.querySelector(".menu-content");
-    console.log(content.style.display);
+    // console.log(content.style.display);
 };
